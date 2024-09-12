90e66a509ad79045018a8e4302a9ae1b01135771
Shp981 <shuertas981@gmail.com>
2024-09-12 15:59:02 -0500

    Agregando estructura de api con spring boot

---
+++ api/src/test/java/com/factulink/fkb/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.factulink.fkb.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
