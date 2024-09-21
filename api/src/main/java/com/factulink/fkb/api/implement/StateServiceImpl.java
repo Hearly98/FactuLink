87b7a899010bfb680ac7399cfdd143bcacf2a536
jean <jeanct0310@gmail.com>
2024-09-20 23:13:16 -0500

    optimizando errores

--- api/src/main/java/com/factulink/fkb/api/implement/StateServiceImpl.java
+++ api/src/main/java/com/factulink/fkb/api/implement/StateServiceImpl.java
@@ -3,12 +3,12 @@ package com.factulink.fkb.api.implement;
 import com.factulink.fkb.api.model.State;
 import com.factulink.fkb.api.repository.StateRepository;
 import com.factulink.fkb.api.service.StateService;
+import jakarta.persistence.EntityManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
-import javax.persistence.EntityManager;
 import java.util.List;
 
 @Service
